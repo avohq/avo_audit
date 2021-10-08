662882623cfc8749d7baab6e3f7a87e1b0f1386c
bjorn <bjorn@avo.sh>
2021-10-08 14:24:18 +0000

    2 tables with counts merged into 1 and ready compare two time periods together and produce results

--- macros/audit_event_volume.sql
+++ macros/audit_event_volume.sql
@@ -7,15 +7,14 @@
 {%- call statement('total_events', fetch_result=True) -%}
     select source,  count(source)  from {{ volume_relation }} where DATE(ts) = {{ dbt_date.n_days_ago(days_ago) }} group by source
 {%- endcall -%}
-
+ 
 {%- set query = load_result('total_events') -%}
 {%- set query_data = query['data'] -%}
 {% if execute %}
-  {%- set total_events  = query['data'] -%}
-{% else %}
+  {%- set total_events = query['data'] -%}
+  {% else %}
   {%- set total_events = [] -%}
 {% endif %}
-
 
 {{return(total_events)}}
 
@@ -49,7 +48,6 @@ select
     ELSE 0 END AS total_source_events
      {% endif %}
     {% endfor %}
-    -- (count(event_name) / {{total_events_1[source]}}) as ratio
   from {{volume_relation}}
   where DATE(ts) = {{ dbt_date.n_days_ago(5) }}
   group by
@@ -63,39 +61,64 @@ select
     source,
     max(version) as version,
     DATE(ts) as day,
-    count(event_name) as event_count
-    -- {{total_events_2[source]}} as total_events,
-    -- (count(event_name) / {{total_events_2[source]}}) as ratio
+    count(event_name) as event_count,
+    {% for tuple in total_events_2 %}
+    {% if loop.first %}
+      CASE WHEN source = '{{tuple[0]}}' then {{tuple[1]}}
+    {% elif not loop.last %}
+     WHEN source = '{{tuple[0]}}' then {{tuple[1]}}
+    {% else %}
+    WHEN source = '{{tuple[0]}}' then {{tuple[1]}}
+    ELSE 0 END AS total_source_events
+     {% endif %}
+    {% endfor %}
   from {{volume_relation}}
   where DATE(ts) = {{ dbt_date.n_days_ago(6) }}
   group by
   event_name,
   source,
   day
+),
+
+union_query as (
+select event_name,
+ source,
+ day,
+ SUM(event_count) as event_count,
+ SUM(total_source_events) as event_source_count,
+ SUM(event_count / total_source_events) as ratio,
+ MAX(version) as version
+ from query1
+ GROUP BY
+   event_name,
+   source,
+   day
+UNION ALL (
+  select event_name,
+ source,
+ day,
+ SUM(event_count) as event_count,
+ SUM(total_source_events) as event_source_count,
+ SUM(event_count / total_source_events) as ratio,
+MAX(version) as version,
+ from query2
+ GROUP BY
+   event_name,
+   source,
+   day
+)
 )
 
-select * from query1
 
+select * from union_query
 -- select 
---   CASE WHEN query1.event_name IS NOT NULL THEN query1.event_name ELSE query2.event_name END as event,
---   CASE WHEN query1.source IS NOT NULL THEN query1.source ELSE query2.source END as source,
---   MAX(CASE WHEN query1.version IS NOT NULL THEN query1.version ELSE query2.version END) as version,
---   MAX(query1.day) as day_1,
---   SUM(query1.event_count) as event_count_1,
---   {{total_events_1[source]}} as total_events_1,
---   SUM(query1.ratio) as ratio_1,
---   MAX(query2.day) as day_2,
---   SUM(query2.event_count) as event_count_2,
---   {{total_events_2[source]}} as total_events_2,
---   SUM(query2.ratio) as ratio_2
--- from 
---   query1
--- FULL JOIN query2 ON (query1.event_name = query2.event_name AND query1.source = query2.source)
--- GROUP BY
---   event,
---   source
--- ORDER BY 
--- event
+-- a.event_name,
+-- a.source,
+-- a.day as day_1,
+-- b.day as day_2,
+-- a.ratio as ratio_1,
+-- b.ratio as ratio_2,
+-- SUM(a.ratio - b.ratio) // Ready to start comparing ratios with some margins in place. 
 
 
 {% endmacro %}
