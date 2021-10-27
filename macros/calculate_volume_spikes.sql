de0a157da15af1af9b7a3b50a9e56ba199234179
bjorn <bjorn@avo.sh>
2021-10-27 13:40:05 +0000

    getting good data out of the macro now

--- macros/calculate_volume_spikes.sql
+++ macros/calculate_volume_spikes.sql
@@ -1,5 +1,6 @@
 {% macro calculate_volume_spikes(table_ref, sensitivity_percentage) %}
 
+{% set threshold = 3.5 %}
 
 with avarage as (
 select
@@ -15,33 +16,51 @@ GROUP BY
 event_name,
 source
 ),
-x
 groupQuery as (
 select 
   t.event_name as event_name,  
   t.source as source,
-  ARRAY_AGG(DISTINCT t.day 
-            ORDER BY t.day ASC) as days,
-  ARRAY_AGG(t.event_count
-            ORDER BY t.day ASC) as event_counts,
-  ARRAY_AGG(t.event_source_count
-            ORDER BY t.day ASC) as source_counts,
-  ARRAY_AGG(
-    t.percentage
-    ORDER BY t.day ASC) as percentages,
+  t.day as day,
+  MAX(t.event_count) as event_count,
+  MAX(t.event_source_count) as total_source_count,
+  MAX(t.version) as version,
+  MAX(t.percentage) as percentage,
  MAX(m.avg_percentage) as avg_percentage,
- MAX(m.std_percentage) as std_percentage
+ MAX(m.std_percentage) as std_percentage,
+CASE when MAX(t.percentage) > MAX(m.avg_percentage) + MAX(m.std_percentage) * {{threshold}} then 1
+when MAX(t.percentage) < MAX(m.avg_percentage) - MAX(m.std_percentage) * {{threshold}} then -1
+ELSE 0
+end as signal
   from {{table_ref}} t
   LEFT JOIN avarage m
   ON t.event_name = m.event_name AND t.source = m.source
   group by
   t.event_name,
-  t.source
+  t.source, 
+  t.day
+),
+
+signal_query as (
+select 
+  event_name,
+  source,
+  ARRAY_AGG(day order by day ASC) as days,
+  ARRAY_AGG(event_count order by day ASC) as event_counts,
+  ARRAY_AGG(total_source_count order by day ASC) as total_events_on_source, 
+  ARRAY_AGG(percentage order by day ASC) as percentages,
+  ARRAY_AGG(signal order by day ASC) as signals,
+  from groupQuery
+  GROUP BY
+    event_name,
+    source
 )
 
-select 
- *
-  from groupQuery
+
+select * from signal_query
+ WHERE
+    (select signal FROM UNNEST(signals) AS signal where signal = 1 GROUP BY signal) = 1
+    OR (select signal from UNNEST(signals) AS signal where signal = -1 GROUP BY signal) = -1
+  
 
 
 {% endmacro %}
