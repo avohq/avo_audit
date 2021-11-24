46c7b49b5bc7d76881493cf51564823163c2ca48
bjorn <bjorn@avo.sh>
2021-11-24 11:00:47 +0000

    integration tests connected with biguquery CI

--- integration_tests/models/experiment_test_data.sql
+++ integration_tests/models/experiment_test_data.sql
@@ -1,1 +1,15 @@
-select * from {{ref('avo_audit_test_data')}}
+
+{% set end_date_script = modules.datetime.date(2021, 11, 04) %}
+{% set today = modules.datetime.date.today() %}
+{% set delta = today - end_date_script %}
+
+{% set end_date = dbt_date.n_days_ago(delta.days) %}
+{%- set days_back = 10 -%}
+{%- set days_lag = 5 -%}
+{%- set event_name_column = 'event' -%}
+{%- set event_date_column = 'sent_at' -%}
+{%- set event_source_column = 'client' -%}
+
+{{
+  avo_audit.audit_event_volume(ref('avo_audit_test_data'), end_date, days_back, days_lag, event_name_column, event_date_column, event_source_column)
+}}
